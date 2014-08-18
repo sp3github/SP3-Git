e676b4f9aec83341598370ca686f07e1da6294ac
ShumWengSang <ballwallcallall@gmail.com>
2014-08-18 15:24:38 +0800

    Added in Player Class and Map Class
    Only added in the classes for the player and the map.

---
+++ DM2231_Lab2_MVC/DM2231_Lab2/Header.h
@@ -0,0 +1,8 @@
+#pragma comment(lib, "opengl32.lib")
+#pragma comment(lib, "glu32.lib")
+
+#include <stdio.h>
+#include <windows.h> // Header File For Windows
+
+#include <gl\GL.h>
+#include <gl\GLU.h>
