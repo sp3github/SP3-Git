ee149b97dbd98380db27f00f1ddd64ce6cf45d1d
WANWENN <w-anwen@hotmail.com>
2014-08-20 16:10:47 +0800

    Entity Factory
    Entity Factory class , Ammo class , health class , slowdown class, items
    class

--- DM2231_Lab2_MVC/DM2231_Lab2/DM2231_Model.h
+++ DM2231_Lab2_MVC/DM2231_Lab2/DM2231_Model.h
@@ -1,6 +1,7 @@
 #pragma once
-
+#include <vector>
 #include <stdio.h>
+#include "Entity.h"
 
 class DM2231_Model
 {
@@ -9,4 +10,5 @@ public:
 	~DM2231_Model(void);
 	// Update the model
 	void Update(void);
+	std::vector<CEntity> theArrayOfEntities;
 };
